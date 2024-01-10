c37121e01fc9d1858d7cc5a4110f80828033e529
Mehmet Güven <92626875+xnymerian@users.noreply.github.com>
2024-01-10 19:59:57 +0300

    A simple Mvc Web Hospital Information Project was created
    A Hospital Information System was created with the help of ASP.net Entity Framework using a simple database and MVC template.

---
+++ webtekno/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Razor.Generator;
+using webtekno.Models.Entity;
+using webtekno.Models.entity3;
+
+
+namespace webtekno.Controllers
+{
+    public class HomeController : Controller
+    {
+        hastadb2Entities db = new hastadb2Entities();
+        hastadb2Entities4 db1= new hastadb2Entities4();
+        
+        public ActionResult Index()
+        {
+            var degerler= db.hastabilgi1.ToList();
+            return View(degerler);
+            
+        }
+        [HttpGet]
+        public ActionResult About()
+        {
+            
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult About( hastabilgi1 TBL )
+        {
+           db.hastabilgi1.Add( TBL );
+            db.SaveChanges();
+
+            return View();
+        }
+
+        
+        public ActionResult Contact(string p)
+        {
+            var degerler = from d in db.hastabilgi1 select d;
+            if (!string.IsNullOrEmpty(p))
+            {
+                degerler = degerler.Where(m => m.hastaad.Contains(p));
+            }
+
+            return View(degerler.ToList());
+
+            
+        }
+        
+        public ActionResult SIL(int id)
+        {
+            var kategori = db.hastabilgi1.Find(id);
+            db.hastabilgi1.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Doctors()
+        {
+            var degerler = db1.doktorlar.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult Doctors1()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Doctors1(doktorlar DR)
+        {
+            db1.doktorlar.Add(DR);
+            db1.SaveChanges();
+            return View();
+        }
+        public ActionResult Doctors2(string p)
+        {
+            var degerler = from d in db1.doktorlar select d;
+            if (!string.IsNullOrEmpty(p))
+            {
+                degerler = degerler.Where(m => m.doktorad.Contains(p));
+            }
+
+            return View(degerler.ToList());
+            
+        }
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ webtekno/Controllers/SecurityController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using webtekno.Models.entity2;
+
+
+
+namespace webtekno.Controllers
+{
+    [AllowAnonymous]
+    public class SecurityController : Controller
+    {
+
+        hastadb2Entities3 db = new hastadb2Entities3();
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(sifreler user)
+        {
+            
+            var userIndb = db.sifreler.FirstOrDefault(x => x.kullaniciadi == user.kullaniciadi && x.sifre == user.sifre);
+            
+             
+            if (userIndb == null )
+            {
+                ViewBag.Mesaj = "Gecersiz Kullanıcı.Kullanıcı adı veya şifre hatalı.";
+                FormsAuthentication.SetAuthCookie(user.kullaniciadi, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Mesaj = "Gecersiz Kullanıcı.Kullanıcı adı veya şifre hatalı.";
+                return View();
+            }
+            
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+        [HttpPost]
+        public ActionResult Register(sifreler user)
+        {
+            db.sifreler.Add(user);
+            db.SaveChanges();
+            return View();
+        }
+        
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+
+}
